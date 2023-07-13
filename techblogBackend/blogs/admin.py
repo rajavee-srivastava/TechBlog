a9bc51e472326520af409ba984d5a246a27d9080
rajavee-srivastava <rajaveesrivastava@gmail.com>
2023-07-14 01:59:28 +0530

    created models and registered on the admin site.

--- techblogBackend/blogs/admin.py
+++ techblogBackend/blogs/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Category, Post, Project, Image 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','created_at', 'category')
+    search_fields = ('title','created_at','category')
+    list_filter = ('created_at',)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title','created_at')
+    search_fields = ('title','created_at')
+    list_filter = ('created_at',)
+
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Project, ProjectAdmin)
+
--- techblogBackend/blogs/models.py
+++ techblogBackend/blogs/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    short_description = models.CharField(max_length=300)
+    content = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    project_link = models.URLField()
+    images = models.ManyToManyField('Image')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    url = models.URLField()
+
+    def __str__(self):
+        return self.url
